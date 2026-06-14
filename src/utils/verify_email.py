50f9db870a12c1b6a97f4bc8391689acbe39f610
Faizan Ansari <md.faizan8998@gmail.com>
2026-06-14 15:48:35 +0530

    Update verify_email.py

--- src/utils/verify_email.py
+++ src/utils/verify_email.py
@@ -23,8 +23,9 @@ Todo App Team
 """)
     
 
-   with smtplib.SMTP("smtp.gmail.com", 587, timeout=15) as smtp:
-        smtp.starttls()
+  with smtplib.SMTP("smtp.gmail.com", 587, timeout=15) as smtp:
+        smtp.starttls() # 🔥 Yeh line connection ko secure karegi (TLS)
+        smtp.login(
             "md.faizan8998@gmail.com",
             "scyr beod baif ukif"
         )
